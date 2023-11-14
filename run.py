d14a9ca50285a002512a2e30eb41dd43736cea24
jimjonesbabyfreshout <127491166+jimjonesbabyfreshout@users.noreply.github.com>
2023-11-14 11:01:06 -0800

    Update run.py

--- run.py
+++ run.py
@@ -13,6 +13,7 @@ def banner():
 while n>0:
     banner()
     print("CTRL + z for exit")
-    cou = input("\033[1;32m Enter your country code\033[1;34m:\033[1;31m➤")
-    num = input("\033[1;32m Enter your number \033[1;34m:\033[1;31m➤")
-    os.system("python2 tracker.py"+" "+cou+num)
+    cou = eval(input("\033[1;32m Enter your country code\033[1;34m:\033[1;31m➤"))
+    num = eval(input("\033[1;32m Enter your number \033[1;34m:\033[1;31m➤"))
+    os.system("python3 tracker.py"+" "+cou+num)
+
